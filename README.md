04541800c85fe1f3ca347f0298782d4e20d5c94e
Jonathan E. Magen <yonkeltron@gmail.com>
2016-05-14 21:05:07 -0400

    additional files
    Signed-off-by: Jonathan E. Magen <yonkeltron@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+This is an experiment in concurrent CSV processing.
+
+Languages used as parts of the experiment:
+
+- Scala
+- Awk
+- Ruby
---
+++ fake_data.rb
@@ -0,0 +1,27 @@
+require 'csv'
+
+letter_range = ('a'..'d').to_a * 3
+l = letter_range.length
+n = 10
+
+header = ('a'..'j').to_a
+
+puts "Generating character sequences"
+CSV.open('char_sequences.csv', 'wb') do |csv|
+  csv << header
+  10_000_000.times do |i|
+    csv << letter_range.sample(n)
+    print '.' if i % 10_000 == 0
+  end
+end
+
+# puts "Generating numeric sequences"
+# numeric_range = (1..4).to_a * 3
+# CSV.open('num_sequences.csv', 'wb') do |csv|
+#   csv << header
+#   100_000.times do |i|
+#     csv << numeric_range.sample(n)
+#   end
+# end
+
+puts 'Done.'
