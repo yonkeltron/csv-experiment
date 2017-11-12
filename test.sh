6ae1097896bdf71196d004519f9fb101c8e47668
Jonathan E. Magen <yonkeltron@gmail.com>
2017-11-12 12:59:00 -0500

    getting ready for talk
    Signed-off-by: Jonathan E. Magen <yonkeltron@gmail.com>

--- test.sh
+++ test.sh
@@ -3,22 +3,22 @@
 trap 'exit 1' ERR
 
 echo
-echo '==> awk'
-time cat char_sequences.csv | /usr/bin/awk -f process.awk > panda.csv
-echo
-echo '==> gawk'
-time cat char_sequences.csv | gawk -f process.awk > panda.csv
-echo
-echo '==> Synchronous'
+# echo '==> awk'
+# time cat char_sequences.csv | /usr/bin/awk -f process.awk > panda.csv
+# echo
+# echo '==> gawk'
+# time cat char_sequences.csv | gawk -f process.awk > panda.csv
+# echo
+echo '==> Scala: Synchronous'
 time java -jar ./target/scala-2.11/CSV\ Handler-assembly-1.0.jar -m 'sync' -i char_sequences.csv > panda.csv
 echo
-echo '==> Akka Actors'
-time java -jar ./target/scala-2.11/CSV\ Handler-assembly-1.0.jar -m 'actor' -i char_sequences.csv > panda.csv
-echo
-echo '==> Scala Parallel collections'
+# echo '==> Akka Actors'
+# time java -jar ./target/scala-2.11/CSV\ Handler-assembly-1.0.jar -m 'actor' -i char_sequences.csv > panda.csv
+# echo
+echo '==> Scala: Parallel collections'
 time java -jar ./target/scala-2.11/CSV\ Handler-assembly-1.0.jar -m 'pc' -i char_sequences.csv > panda.csv
 echo
-echo '==> Akka Streams'
+echo '==> Scala: Akka Streams'
 time java -jar ./target/scala-2.11/CSV\ Handler-assembly-1.0.jar -m 'stream' -i char_sequences.csv > panda.csv
 echo
 echo
